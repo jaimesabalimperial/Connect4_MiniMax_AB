ac342c52523f42397c3cab4ca92fde3a58f3a237
Jaime Sabal <jsabalb@gmail.com>
2021-11-30 23:09:33 +0000

    Added comments to code, made everything neat to turn in

--- simulation.py
+++ simulation.py
@@ -1,6 +1,6 @@
-from board_C4 import Board
-from game_C4 import Game
-from player_C4y import AutomaticPlayer, ManualPlayer, RandomPlayer
+from board import Board
+from game import Game
+from player import ManualPlayer, AlphaBetaPlayer, MiniMaxPlayer
 
 class ManualVsManualSimulation:
     """ Play against your friend (or more likely yourself)! """
@@ -16,35 +16,7 @@ class ManualVsManualSimulation:
         game.play()
 
 
-class ManualVsRandomSimulation:
-    """ Try to defeat a RandomPlayer! """
-    def run(self):
-
-        # Creating a manual player
-
-        alice = ManualPlayer(name="Paul Bilokon (Manual)")
-        
-        # Creating a random player
-        bob = RandomPlayer(name="Bob (Random)")
-
-        # Creating and launching the game
-        game = Game(player1=alice, player2=bob)
-        game.play()
-
-
-class RandomVsRandomSimulation:
-    """ Two RandomPlayers battling it out! Whose will have the better luck? """
-    def run(self):
-        # Creating two random players
-        alice = RandomPlayer(name="Alice (Random)")
-        bob = RandomPlayer(name="Bob (Random)")
-
-        # Creating and launching the game
-        game = Game(player1=alice, player2=bob)
-        game.play()
-
-
-class ManualVsAutomaticSimulation:
+class ManualVsAlphaBetaSimulation:
     """ Play against your AI player! """
     def run(self):
         # Creating a manual player with automatically generated board
@@ -52,31 +24,34 @@ class ManualVsAutomaticSimulation:
         alice = ManualPlayer(board=board, name="Alice (Manual)")
         
         # Creating a manual player
-        bob = AutomaticPlayer(name="Bob (Automatic)")
+        bob = AlphaBetaPlayer(name="Bob (Automatic)")
 
         # Creating and launching the game
         game = Game(player1=alice, player2=bob)
         game.play()
 
 
-class RandomVsAutomaticSimulation:
-    """ A RandomPlayer vs your smarter AI player! """
+class ManualVsMiniMaxSimulation:
+    """ Play against your AI player! """
     def run(self):
-        # Creating one random player and one AI player
-        alice = RandomPlayer(name="Alice (Random)")
-        bob = AutomaticPlayer(name="Bob (Automatic)")
+        # Creating a manual player with automatically generated board
+        board = Board()
+        alice = ManualPlayer(board=board, name="Alice (Manual)")
+        
+        # Creating a manual player
+        bob = MiniMaxPlayer(name="Bob (Automatic)")
 
         # Creating and launching the game
         game = Game(player1=alice, player2=bob)
-        game.play()
+        game.play() 
 
 
 class AutomaticVsAutomaticSimulation:
     """ Get your AI players to battle each other! """
     def run(self):
         # Creating two AI players
-        alice = AutomaticPlayer(name="Alice (Automatic)")
-        bob = AutomaticPlayer(name="Bob (Automatic)")
+        alice = AlphaBetaPlayer(name="Bob (Automatic)")
+        bob = AlphaBetaPlayer(name="Alice (Automatic)")
 
         # Creating and launching the game
         game = Game(player1=alice, player2=bob)
